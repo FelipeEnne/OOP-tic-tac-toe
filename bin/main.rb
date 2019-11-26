b2f95dd1a6ddb4bb8da43e44f1410b6f01d569b4
300ms <ilhansnmz44@gmail.com>
2019-11-26 18:32:42 +0300

    User interface messages and imput commands have been added

--- bin/main.rb
+++ bin/main.rb
@@ -2,4 +2,50 @@
 
 # frozen_string_literal: true
 
-puts "Hello Word!"
+# rubocop: disable Metrics/MethodLength
+
+class TicTacToe
+  @table = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+  def put_table
+    @table.each do |x|
+      x.each do |y|
+        if y == 1
+          print ' O '
+        elsif y == 2
+          print ' X '
+        else
+          print ' _ '
+        end
+      end
+      puts ' '
+    end
+  end
+
+  def opening
+    puts 'Game has started now! Player 1 turn ..'
+  end
+
+  def move(player)
+    puts "#{player} coordinates:"
+    coordinates = gets.chomp
+  end
+
+  def error_message
+    puts 'Error: coordinates are occupied'
+  end
+
+  def error_coordinates
+    puts 'Error: coordinates are not between 1-3'
+  end
+
+  def winner(player)
+    print "Player#{player} Wins !"
+  end
+
+  def no_cells
+    puts 'No more cells !'
+  end
+end
+
+# rubocop: enable Metrics/MethodLength
