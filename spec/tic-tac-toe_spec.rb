911cb9447f6c3828bbc526fafca6603e6586b97f
300ms <ilhansnmz44@gmail.com>
2019-11-29 00:55:27 +0300

    Some file errors are fixed

--- spec/tic-tac-toe_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/main_test'
-
-
-describe "#initialize" do
-  it "assigns name to a player" do
-    expect(name1).to eql('ilhan')
-    expect(name2).to eql('felipe')
-  end
-end
